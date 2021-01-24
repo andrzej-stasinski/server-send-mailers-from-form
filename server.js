542a158a2db5513eb078e3ec00552f4198a9c3d4
Andrzej Stasinski <andis_as@wp.pl>
2021-01-24 20:35:51 +0100

    correct

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-console.log('Server Express')
-
 const express = require('express')
 const nodemailer = require('nodemailer')
 require('dotenv').config()
+
+console.log('Server Express')
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -27,7 +27,7 @@ const SUBJECT = 'mail from Andrzej'
 app.get('/message', (req, res) => {
     console.log(req.query)
     const {message, email} = req.query 
-    console.log(message +' '+ email)
+    console.log('message & email ', message, email)
     if(!message) res.status(400)
     if(!email) res.status(400)
     res.send( '<h1>Server:</h1>'+email+'<br/>'+SUBJECT+'<br/>'+message )
@@ -40,7 +40,7 @@ app.get('/message', (req, res) => {
     },
     (err, info) => {
       if(err) {
-        console.log('err', err)
+        console.log('callback', err, info)
         return res.status(500).send()
       }
       console.log('info', info)
